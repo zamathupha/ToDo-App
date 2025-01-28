6737ace379f35e9ab5dea1476ac18f001c51c789
Zama Thupha <zamathupha@gmail.com>
2025-01-28 08:50:49 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader,
+   IonToolbar,
+    IonTitle,
+     IonContent,
+    IonItem,
+    IonLabel,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonIcon,
+    
+  } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [IonHeader,IonToolbar, IonItem,IonContent,IonLabel,
+   
+],
 })
 export class HomePage {
-  constructor() {}
+  newTask: string = '';
+  tasks: Array<{ name: string, completed: boolean }> = [];
+  i: number = 0;
+
+  addTask() {
+    if (this.newTask.trim().length === 0) {
+      return;
+    }
+    this.tasks.push({
+      name: this.newTask,
+      completed: false
+    });
+    this.newTask = '';
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+  
 }
+
+
+
+
+
